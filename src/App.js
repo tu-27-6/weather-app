62af93f7037d2e6f3e2868f8e120778a46945c62
tu-27-6 <ntu2762001@gmail.com>
2022-04-07 23:46:37 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import './style.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+export default function App() {
+    const [weather, setWeather] = useState({})
+    const [city, setCity] = useState('')
+
+    const apiKey = 'd6742feb87031d6e10bb0ad16da1bbea'
+
+    const getWeather = (e) => {
+        if(e.key === 'Enter') {
+            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+                .then(res => res.json())
+                .then(result => {
+                    setWeather(result)
+                    setCity('')
+                })
+        }
+    }
+
+    return (
+        <div className='app'>
+            <div className='container'>
+                <h1>Weather App</h1>
+                <input 
+                    type='text'
+                    placeholder='Search a city'
+                    value={city}
+                    onChange={(e) => setCity(e.target.value)}
+                    onKeyPress={getWeather}
+                />
+
+            {
+                typeof weather.main != 'undefined' ? 
+                (
+                    <div className='weather-container'>
+                        <div className='location'>
+                            {weather.name}, {weather.sys.country}
+                        </div>
+
+                        <div className='temp'>
+                            {Math.round(weather.main.temp)}°C
+                        </div>
+
+                        <div className='weather'>
+                            {weather.weather[0].main}
+                        </div>
+                    </div>
+                )
+                :
+                (
+                    <div className='weather-container'>
+
+                    </div>
+                )
+            }
+
+            </div>
+        </div>
+    )
 }
-
-export default App;
